9793e7a0b5c3d028e08d0c3f61f95ff7bdcc3546
Tsubasa Kubo <nepia.infinity12335@gmail.com>
2026-06-26 14:37:59 +0900

    Initial commit

---
+++ manifest.ts
@@ -0,0 +1,29 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import { def as detectLang } from "./functions/detect_lang.ts";
+import { def as translate } from "./functions/translate.ts";
+import reacjilator from "./workflows/reacjilator.ts";
+
+/**
+ * DeepL Translator app translates Slack messages in channels.
+ * During the open beta period, only public channels are supported.
+ * To run this app, DeepL API account is required.
+ * Refer to https://www.deepl.com/en/docs-api for DeepL API details.
+ */
+export default Manifest({
+  name: "reacjilator-app",
+  description: "A Slack app translates Slack messages",
+  icon: "assets/default_new_app_icon.png",
+  functions: [detectLang, translate],
+  workflows: [reacjilator],
+  outgoingDomains: [
+    "api-free.deepl.com",
+    "api.deepl.com",
+  ],
+  botScopes: [
+    "commands",
+    "chat:write",
+    "channels:history",
+    "groups:history",
+    "reactions:read",
+  ],
+});
---
+++ triggers/reaction_added_trigger.ts
@@ -0,0 +1,28 @@
+import type { Trigger } from "deno-slack-sdk/types.ts";
+import {
+  TriggerContextData,
+  TriggerEventTypes,
+  TriggerTypes,
+} from "deno-slack-api/mod.ts";
+import workflowDef from "../workflows/reacjilator.ts";
+
+/**
+ * This is a sample trigger demonstrating how to configure the "reaction_added" event trigger.
+ */
+const trigger: Trigger<typeof workflowDef.definition> = {
+  type: TriggerTypes.Event,
+  name: "Reaction added event trigger",
+  description: "A trigger to start a new workflow",
+  workflow: `#/workflows/${workflowDef.definition.callback_id}`,
+  event: {
+    event_type: TriggerEventTypes.ReactionAdded,
+    all_resources: true,
+  },
+  inputs: {
+    channelId: { value: TriggerContextData.Event.ReactionAdded.channel_id },
+    messageTs: { value: TriggerContextData.Event.ReactionAdded.message_ts },
+    reaction: { value: TriggerContextData.Event.ReactionAdded.reaction },
+  },
+};
+
+export default trigger;
